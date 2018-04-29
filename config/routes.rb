cf4bfcafa4187c2ee68b4b7a9ff7a4d9658e3a92
phoebes828 <phoebe.azucena@outlook.com>
2018-04-29 17:08:13 -0400

    5.1 Reroute root page to main index page, redirect users to landing page when updating a product, create dynamic links on landing page, and updated dynamic links on nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
+
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
 
   get 'simple_pages/index'
-  root 'simple_pages#landing_page'
+
+  get 'simple_pages/landing_page'
+  root 'simple_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
