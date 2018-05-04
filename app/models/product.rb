17bc0cc4bcdd61d9e5b5312114ffad871c7a186c
phoebes828 <phoebe.azucena@outlook.com>
2018-05-04 16:09:08 -0400

    5.2 Create a search form for products, style products edit form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   has_many :orders
+  def self.search(search_term)
+    like_string = Rails.env.production? ? "ilike" : "LIKE"
+    Product.where("name LIKE ?", "%#{search_term}%")
+  end
 end
