12c6371ea6aa0621ef03e98ebdd4915f35a5039a
phoebes828 <phoebe.azucena@outlook.com>
2018-05-13 18:29:40 -0400

    5.5 Add page authorization for user login and edit alerts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user! # User must be signed in
+  load_and_authorize_resource
   # GET /users
   # GET /users.json
   def index
