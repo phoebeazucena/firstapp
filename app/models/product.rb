86f793123a3a0f6946a3991919a74c0fca12add1
phoebes828 <phoebe.azucena@outlook.com>
2018-05-17 19:29:24 -0400

    5.7 Create feature branch for comments, create comment model, add scopes/custom methods, and style products/show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,14 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
   def self.search(search_term)
     like_string = Rails.env.production? ? "ilike" : "LIKE"
     Product.where("name LIKE ?", "%#{search_term}%")
+  end
+  def highest_rating_comment
+    comments.rating_desc.first&.rating
+  end
+  def lowest_rating_comment
+    comments.rating_asc.first&.rating
   end
 end
