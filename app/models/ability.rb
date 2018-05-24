a3b43752c2b6ba221eaf923e58221b02199dffec
phoebes828 <phoebe.azucena@outlook.com>
2018-05-24 19:36:44 -0400

    5.10 Restrict destroy ability to admin only

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,9 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
     can :manage, User, id: user.id
   end
 end
