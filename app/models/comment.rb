c577644e0dcfd4e7e48b879c9f4240c3c8b87072
phoebes828 <phoebe.azucena@outlook.com>
2018-05-24 19:14:59 -0400

    5.10 Add admin flag to users and ability to delete comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,5 +8,5 @@ class Comment < ApplicationRecord
   validates :body, presence: true
   validates :user, presence: true
   validates :product, presence: true
-  # validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { only_integer: true }
 end
