4917b15010c8c3b6888096aecad3466b958a1236
phoebes828 <phoebe.azucena@outlook.com>
2018-05-24 19:54:55 -0400

    5.10 Re-deploying to Heroku after app crash

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.admin?
-      can :manage, :all
+      can :manage, :all # allows admin to make all modifications 
     else
     can :manage, User, id: user.id
   end
