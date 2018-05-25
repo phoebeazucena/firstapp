a934d32e7013bf3188ea068c333c63cb8b9c8d33
phoebes828 <phoebe.azucena@outlook.com>
2018-05-24 20:05:25 -0400

    5.10 Fixed typo in ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,9 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.admin?
-      can :manage, :all # allows admin to make all modifications 
+      can :manage, :all # allows admin to make all modifications
     else
     can :manage, User, id: user.id
+    end
   end
 end
