cf40f96642b728ca95638db632c7de9a00ee3e99
phoebes828 <phoebe.azucena@outlook.com>
2018-05-30 11:07:04 -0400

    5.11 Improve layout of nav bar and add auto email on sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,4 +5,9 @@ class UserMailer < ApplicationMailer
   @message = message
     mail(from: email, to: 'phoebe.azucena@outlook.com', subject: "Contact form message from #{name}")
   end
+
+  def welcome(user)
+    @appname = "Salt of the Sea"
+    mail(to: user.email, subject: "Welcome to #{@appname}!")
+  end
 end
